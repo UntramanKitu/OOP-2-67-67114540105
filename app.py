f36d8ad317a03f15f136c2e31bb1f41474f2a707
UntramenKitu <lnkgame900@gmail.com>
2025-01-24 14:04:00 +0700

    สร้าง class แรกบน Github repo 555

---
+++ app.py
@@ -0,0 +1,12 @@
+class Student() :
+  student_count = 0
+
+  def _new_(self):
+    print('Student._new_')
+    
+  def _init_(self)
+    print('Student._init_')
+    self.gpa = 4.00
+
+  def study_hard(self):
+    print('Sir, yes sir.')
